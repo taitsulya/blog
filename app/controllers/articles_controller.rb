b6819f7c58732be90f7585f9fadfa1578c5fa0e7
taitsulya <iojibka_2000@mail.ru>
2021-08-20 04:33:56 +0300

    Add around_action and after_action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+  after_action :show_changes, only: %i[create update destroy]
+  around_action :info
+
   def index
     @articles = Article.all
   end
@@ -43,6 +46,30 @@ class ArticlesController < ApplicationController
   end
 
   private
+
+  def show_changes
+    puts 'Article: '
+    @article.attributes.each { |key, value| puts "  #{key}: #{value}" }
+  end
+
+  def info
+    before
+    yield
+    after
+  end
+
+  def before
+    before = "BEFORE ACTION\n"
+    before << "Request method: #{request.method}\nIP: #{request.ip}\nPath: #{request.path}\nRequest parameters:\n"
+    request.params.each { |key, value| before << "  #{key}: #{value}\n" }
+    puts before
+  end
+
+  def after
+    after = "AFTER ACTION\n"
+    after << "Response status: #{response.status} #{response.message}"
+    puts after
+  end
 
   def article_params
     params.require(:article).permit(:title, :body)
