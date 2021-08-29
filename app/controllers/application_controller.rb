ca3a9fb1b32f9f1784e6ecd3a6afc1b72bada7a8
taitsulya <iojibka_2000@mail.ru>
2021-08-30 02:02:34 +0300

    Move set_locale method into ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,13 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :set_locale
+
   private
+
+  def set_locale
+    I18n.locale = I18n.available_locales.map(&:to_s).include?(params[:locale]) ? params[:locale] : I18n.default_locale
+  end
 
   def default_url_options
     { locale: I18n.locale }
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,6 @@
 
 class ArticlesController < ApplicationController
   before_action :set_format, only: :show_format
-  before_action :set_locale
   after_action :show_changes, only: %i[create update destroy]
   around_action :info
 
@@ -71,11 +70,6 @@ class ArticlesController < ApplicationController
   end
 
   private
-
-  def set_locale
-    locale = I18n.available_locales.map(&:to_s).include?(params[:locale]) ? params[:locale] : nil
-    I18n.locale = locale || I18n.default_locale
-  end
 
   def set_format
     request.format = params[:page_format]
